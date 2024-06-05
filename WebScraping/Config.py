50f8f63c12d4e113bdd9a403b4be68fc3295a825
Nycolas Galdino <nycolaspimentel12@gmail.com>
2024-06-05 10:05:44 -0300

    modified: Altered variables in config.py

--- WebScraping/Config.py
+++ WebScraping/Config.py
@@ -1,2 +1,2 @@
-email = "Insira seu e-mail"  # Insira seu e-mail de contato entre as aspas (ex: npg.softwares@gmail.com)
+user_email = "Insira seu e-mail"  # Insira seu e-mail de contato entre as aspas (ex: npg.softwares@gmail.com)
 password = "Insira sua senha"  # Insira sua senha de e-mail entre as aspas
