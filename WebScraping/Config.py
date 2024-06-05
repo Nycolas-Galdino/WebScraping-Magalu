ec8d1fdfc979e7d8e3c425f64e47cb702180f273
Nycolas Galdino <nycolaspimentel12@gmail.com>
2024-06-05 08:06:17 -0300

    modified: Rm unused variables

--- WebScraping/Config.py
+++ WebScraping/Config.py
@@ -1,4 +1,2 @@
-user = "Insira seu nome"  # Insira seu nome de usuário entre as aspas (ex: Nycolas Galdino)
 email = "Insira seu e-mail"  # Insira seu e-mail de contato entre as aspas (ex: npg.softwares@gmail.com)
 password = "Insira sua senha"  # Insira sua senha de e-mail entre as aspas
-from_email = f"{user} <{email}>"
